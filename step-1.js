d18b92bc8fd82959033bb7b5a4171abfadb5477e
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-12-03 18:19:34 +0000

    finished step 1

---
+++ step-1.js
@@ -0,0 +1,26 @@
+function getAndPrintHTMLChunks () {
+
+  var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+
+
+    response.on('data', function (data) {
+      console.log(data);
+      console.log('/n');
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+}
+
+getAndPrintHTMLChunks();
